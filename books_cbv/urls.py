fd04864ca49a0319d20e5ea28adcebe8fae51300
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 16:01:09 -0300

    Refactor books_cbv/urls.py.

--- books_cbv/urls.py
+++ books_cbv/urls.py
@@ -5,8 +5,15 @@ from . import views
 app_name = 'books_cbv'
 
 urlpatterns = [
-  path('', views.BookList.as_view(), name='book_list'),
-  path('new', views.BookCreate.as_view(), name='book_new'),
-  path('edit/<int:pk>', views.BookUpdate.as_view(), name='book_edit'),
-  path('delete/<int:pk>', views.BookDelete.as_view(), name='book_delete'),
+  path(''
+  , views.BookList.as_view(), name='book_list'),
+  
+  path('new'
+  , views.BookCreate.as_view(), name='book_new'),
+  
+  path('edit/<int:pk>'
+  , views.BookUpdate.as_view(), name='book_edit'),
+  
+  path('delete/<int:pk>'
+  , views.BookDelete.as_view(), name='book_delete'),
 ]
