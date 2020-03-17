cb34f814a958a7ea7f42091eed79567b2a37f631
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 15:59:42 -0300

    Remove old apps from apps/urls.py.

--- apps/urls.py
+++ apps/urls.py
@@ -8,9 +8,4 @@ urlpatterns = [
 
     # Home or http:<site_addr>/
     path('', include('books_cbv.urls', namespace='books_cbv')),
-
-    #path('books_cbv/', include('books_cbv.urls', namespace='books_cbv')),    
-    # path('books_fbv/', include('books_fbv.urls', namespace='books_fbv')),
-    #  path('books_fbv_user/', include('books_fbv_user.urls', namespace='books_fbv_user')),
-    #path('', views.home),
 ]
