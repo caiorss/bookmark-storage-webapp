afd7cd12aefb35d37865a387bfefc8fc3e155481
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 22:33:25 -0300

    Create Tag models.

--- books_cbv/models.py
+++ books_cbv/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 from urllib.parse import urlparse 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=300, unique = True)
+
+    def __str__(self):
+        return self.name 
+
 class SiteBookmark(models.Model):
     # Max URL size has 3000 bytes
     #url   = models.CharField(max_length=4000)
@@ -10,9 +16,10 @@ class SiteBookmark(models.Model):
     name = models.CharField(max_length= 8000, blank = True, null = True)    
     starred = models.BooleanField(blank = True)
     brief = models.TextField(blank = True, null = True)
+    tags = models.ManyToManyField(Tag, blank = True)
 
     def __str__(self):
-        return self.name
+        return "name = {name} ; url = {url}".format(name = self.name, url = self.url)
 
     def get_absolute_url(self):
         return reverse('books_cbv:book_edit', kwargs={'pk': self.pk})
