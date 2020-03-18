5c8a63c86ba41b65e5c59a27301f5430c44e68f8
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 22:35:07 -0300

    Implement search bookmark functionality.

--- books_cbv/views.py
+++ books_cbv/views.py
@@ -2,18 +2,30 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView,ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q 
 
 from books_cbv.models import SiteBookmark
 
+# Template files 
+tpl_main = "books_cbv/book_list.html"
 tpl_forms = "books_cbv/form.html"
 
 class BookmarkList(ListView):
-    template_name = "books_cbv/book_list.html"
+    template_name = tpl_main
     model = SiteBookmark
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        if query:
+            return self.model.objects.filter( Q(name__contains = query) 
+                                            | Q(url__contains = query) )
+        
+        #print(" [BookmarkList] kwargs = " + str(self.kwargs))
+        return self.model.objects.all()
 
 
 class BookmarkStarred(ListView):
-    template_name = "books_cbv/book_list.html"
+    template_name = tpl_main
     # model = SiteBookmark
 
     def get_queryset(self):
@@ -23,13 +35,13 @@ class BookmarkStarred(ListView):
 class BookmarkCreate(CreateView):
     template_name = tpl_forms
     model = SiteBookmark
-    fields = ['name', 'url', 'starred', 'brief']
+    fields = ['name', 'url', 'starred', 'brief', 'tags']
     success_url = reverse_lazy('books_cbv:bookmark_list')
 
 class BookmarkUpdate(UpdateView):
     template_name = tpl_forms
     model = SiteBookmark
-    fields = ['name', 'url', 'starred', 'brief']
+    fields = ['name', 'url', 'starred', 'brief', 'tags']
     success_url = reverse_lazy('books_cbv:bookmark_list')
 
 class BookmarkDelete(DeleteView):
