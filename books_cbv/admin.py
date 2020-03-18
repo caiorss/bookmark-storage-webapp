357915826f5508f3bdd56a57d21a5c177bc4dbf5
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 22:33:48 -0300

    Register tag model.

--- books_cbv/admin.py
+++ books_cbv/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from books_cbv.models import SiteBookmark
+from books_cbv.models import SiteBookmark, Tag
 
 admin.site.register(SiteBookmark)
+admin.site.register(Tag)
