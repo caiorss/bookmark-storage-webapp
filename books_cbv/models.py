7286da7570c2e99494e1649db75486ed3a74a179
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-17 21:20:03 -0300

    Change type of field url of model SiteBookmark.

--- books_cbv/models.py
+++ books_cbv/models.py
@@ -5,7 +5,8 @@ from urllib.parse import urlparse
 
 class SiteBookmark(models.Model):
     # Max URL size has 3000 bytes
-    url   = models.CharField(max_length=4000)
+    #url   = models.CharField(max_length=4000)
+    url = models.URLField()
     name = models.CharField(max_length= 8000, blank = True, null = True)    
     starred = models.BooleanField(blank = True)
     brief = models.TextField(blank = True, null = True)
