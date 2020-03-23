74e498ffcdf8f43fef3dfa85c509965caf76d113
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-23 13:00:33 -0300

    Create class SiteBookmarkAdmin for allowing adding tags to bookmars.

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 import bookmarks.models as bm 
 
+class SiteBookmarkAdmin(admin.ModelAdmin):
+    model = bm.SiteBookmark
+    filter_horizontal = ('tags', )
+
 class CollectionAdmin(admin.ModelAdmin):
     model = bm.Collection
     filter_horizontal = ('item', )
 
-admin.site.register(bm.SiteBookmark)
+admin.site.register(bm.SiteBookmark, SiteBookmarkAdmin)
 admin.site.register(bm.Tag)
 admin.site.register(bm.SavedSearch)
 admin.site.register(bm.Collection, CollectionAdmin)
