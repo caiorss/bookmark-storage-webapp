c19d8938e298e917d9df2ba184db0c175b3fa31e
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-24 01:49:11 -0300

    Refactor URL schema and add collection/list route.

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -9,33 +9,21 @@ urlpatterns = [
 
   # Redirect straight to template 
   # see: https://stackoverflow.com/questions/3402708/
-  path('', dvgb.TemplateView.as_view(template_name = 'index.html'), name = 'home'),
-
-  path('items'
-  , views.BookmarkList.as_view(), name='bookmark_list'),
-
-  path('items/starred'
-  , views.BookmarkStarred.as_view(), name='bookmark_starred'),
-  
-  path('items/new'
-  , views.BookmarkCreate.as_view(), name='bookmark_new'),
-  
-  path('items/edit/<int:pk>'
-  , views.BookmarkUpdate.as_view(), name='bookmark_edit'),
-  
-  path('items/delete/<int:pk>'
-  , views.BookmarkDelete.as_view(), name='bookmark_delete'),
+   path('', dvgb.TemplateView.as_view(template_name = 'index.html'), name = 'home')
+  ,path('items', views.BookmarkList.as_view(), name='bookmark_list')
+  ,path('items/starred', views.BookmarkStarred.as_view(), name='bookmark_starred') 
+  ,path('items/new', views.BookmarkCreate.as_view(), name='bookmark_new')
+  ,path('items/edit/<int:pk>', views.BookmarkUpdate.as_view(), name='bookmark_edit')  
+  ,path('items/delete/<int:pk>', views.BookmarkDelete.as_view(), name='bookmark_delete')
 
   # ----------------------------------------------------#
 
-  path('search/list'
-  , views.SavedSearchList.as_view(), name='bookmark_savedsearch_list'),
-
-  path('search/new'
-  , views.SavedSearchCreate.as_view(), name='bookmark_savedsearch_new'),
+  ,path('search/list', views.SavedSearchList.as_view(), name='bookmark_savedsearch_list')
+  ,path('search/new', views.SavedSearchCreate.as_view(), name='bookmark_savedsearch_new')
  
   #----------------------------------------------------#
 
-  path('collection/new', views.CollectionCreate.as_view(), name = 'bookmark_collection_new')
+  ,path('collection/list', views.CollectionList.as_view(), name='bookmark_collection_list')
+  ,path('collection/new',  views.CollectionCreate.as_view(), name = 'bookmark_collection_new')
 
 ]
