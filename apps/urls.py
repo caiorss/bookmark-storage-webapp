7c3b785c3642ca461b50a35c5d13542b0be91150
CaioRS <caiorss.rodrigues@gmail.com>
2020-03-28 15:22:18 -0300

    Change apps/urls.py for installing authentication.

--- apps/urls.py
+++ apps/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.urls import path, include # new
+
 
 # from theme import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
 
     # Home or http:<site_addr>/
     path('', include('bookmarks.urls', namespace='bookmarks')),
