0aedb684f46e1643bb814032cc4edf048ecc153f
CaioRS <caiorss.rodrigues@gmail.com>
2020-04-04 15:34:56 -0300

    Change user-agent in http response for avoiding forbidden 403.

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -213,7 +213,14 @@ def fetch_itemsnapshot(request: WSGIRequest):
 
     try:
         url: str    = item.url 
-        u           = urllib.request.urlopen(url)
+        req = urllib.request.Request(
+            url, 
+            data=None, 
+            headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        })
+        u = urllib.request.urlopen(req)
+        #req           = urllib.request.urlopen(url)
         f_data: bytes = u.read()
         f_name: str = os.path.basename(urlparse(url).path)
         f_hash: str = hashlib.md5(f_data).hexdigest()
