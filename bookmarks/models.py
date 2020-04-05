247ed5a2c51b110ed4127a3c045f810c60e0caf9
CaioRS <caiorss.rodrigues@gmail.com>
2020-04-05 15:50:30 -0300

    Create FileSnapshot class.

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,6 +3,15 @@ from django.urls import reverse
 from urllib.parse import urlparse 
 import datetime
 import uuid
+from django.http import Http404
+import django.conf
+import django.shortcuts as ds 
+
+import bookmarks.dutils as utils 
+from   bookmarks.dutils import DownloadedFile
+import os 
+
+from typing import Optional
 
 class Tag(models.Model):
     name        = models.CharField(max_length=300, unique = True, help_text="Tag title or name")
@@ -89,11 +98,12 @@ class SiteBookmark(models.Model):
         return "; ".join((self.title or "").split(";")[1:])
 
     def snapshot_exists(self):
-        return self.itemsnapshot_set.exists()
+        #return self.itemsnapshot_set.exists()
+        return self.filesnapshot_set.exists()
 
     def snapshot_file(self):
         # print(" =>>> Here ")
-        sn: ManyRelatedManager = self.itemsnapshot_set.first()        
+        sn: ManyRelatedManager = self.filesnapshot_set.first()        
         if sn is not None: 
             s = str(sn.id) + "/" + sn.fileName
             # print(" snapshot = " + s)
@@ -149,3 +159,64 @@ class ItemSnapshot(models.Model):
             , id = self.id
             , h = self.fileHash )
  
+
+ 
+class FileSnapshot(models.Model):
+    """Store file related to the URL in the database."""
+    # UUID 
+    id = models.UUIDField(primary_key  = True
+                        , editable     = False
+                        , auto_created = True
+                        , default      = uuid.uuid4)
+
+    # Set field only when instance is created
+    created = models.DateField(editable = False, auto_now_add = True, null = True)
+    # Set field only when instance is changed
+    updated = models.DateField(editable = False, auto_now = True, null = True)
+
+    item = models.ManyToManyField(SiteBookmark)
+    # File name 
+    fileName = models.CharField(max_length=5000)
+    # File hash = > Contains crypto-hash signature MD5SUM 
+    fileHash = models.CharField(max_length=100, unique = True)
+    # Contains file media type 
+    fileMimeType = models.CharField(max_length=100)
+
+    @staticmethod
+    def createSnapshot(bookmarkID: int, url: str):
+        item: SiteBookmark = ds.get_object_or_404(SiteBookmark, id = bookmarkID)
+        if not item: raise Http404("Item not found")
+        
+        df: DownloadedFile = utils.download_file(url)
+        media_dir: str     = django.conf.settings.MEDIA_ROOT 
+
+        # Create entry in database 
+        sn = FileSnapshot( fileName = df.fileName
+                            ,fileHash = df.fileHash
+                            ,fileMimeType = df.fileMimeType )
+        sn.save()
+        sn.item.add(item)
+        sn.save()
+        # Create associated file at path: 
+        #  ${MEDIA_ROOT}/<FILE UUID>/<FILE_NAME>
+        file_dir = os.path.join(media_dir, str(sn.id))
+        file_path = os.path.join(file_dir, df.fileName)
+        os.mkdir(file_dir)
+        # Create snapshot file 
+        with open(file_path, 'wb') as f:
+            f.write(df.fileData)
+    
+    def getFilePath(self):
+        media_dir: str = django.conf.settings.MEDIA_ROOT 
+        return os.path.join(media_dir, str(self.id), self.fileName)
+    
+    def readFile(self):
+        file_path = self.getFilePath()
+        with open(file_path, mode = 'rb') as fd:
+            return fd.read()
+
+    def __str__(self):
+        return " file = {file} ; id = {id} ; hash = {h} ".format(
+                file = self.fileName
+                , id = self.id
+                , h = self.fileHash )
