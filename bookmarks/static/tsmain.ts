84ecf0e744cfa3c7a6af3c4bf14b4761012f2c20
CaioRS <caiorss.rodrigues@gmail.com>
2021-04-15 06:24:24 -0300

    Migrate tag functions from javascript to typescript.

--- bookmarks/static/tsmain.ts
+++ bookmarks/static/tsmain.ts
@@ -6,16 +6,17 @@ import { Dialog_Basic, Dialog2_Prompt, Dialog_YesNo
       , DialogForm, Dialog_Notify, Dialog_Datalist_Prompt
         } from "./dialogs.js";
 
+export 
 namespace dom {
 
     // Select DOM element by CSS selector 
-    function select(selector: string) 
+    export function select(selector: string) 
     {
         return document.querySelector(selector)
     }
     
     // Toggle DOM element 
-    function toggle(m: HTMLElement) {
+    export function toggle(m: HTMLElement) {
         if (m == null) { alert(" Error: element not found"); }
         var d = m.style.display;
         var v = window.getComputedStyle(m);
@@ -32,7 +33,7 @@ namespace dom {
         }
     } /* -- End of - DOM_toggle() --- */
 
-    function event_onClicked(selector: string, callback) 
+    export function event_onClicked(selector: string, callback) 
     {
         var elem = document.querySelector(selector);
 
@@ -52,7 +53,7 @@ namespace dom {
     }
 
     /** Redirect URL */
-    export function redirect_url(url) 
+    export function redirect_url(url: string) 
     {
         document.location.href = url;
     }
@@ -105,7 +106,220 @@ async function item_set_starred(checkbox)
 
 }
 
+// Utility function for debugging
+export async function api_item_print(item_id)
+{
+    var token = window["generated_token"];
+    let item = await tsutils.ajax_get("/api2/items/" + item_id, token);
+    console.log(" [TRACE] item = ");
+    console.table( item );
+}
 
+
+
+
+// ========= Tags / T A G S ===============================//
+
+
+async function tag_create()
+{
+    let tag_name = await Dialog2_Prompt.prompt("Enter new tag name:");
+    if(tag_name == "") return;
+    let token = window["generated_token"];
+    
+    let payload = { name: tag_name, description: "" };
+    let resp = await tsutils.ajax_request(HttpMethod.HTTP_POST, "/api/tags", token, payload);
+    
+    if(resp["result"] == "OK")
+    {
+        await Dialog_Notify.notify_ok(resp["message"]);
+        dom.page_refresh();
+    } else {
+        await Dialog_Notify.notify_error(resp["message"]);
+    }
+}
+
+export 
+async function tag_add(item_id: Number)
+{
+    // let last_input = new LocalStorageString("tag-last-input", "");
+        
+    let dlg = new Dialog_Datalist_Prompt();
+    dlg.setTitle("Select a tag");    
+    
+    // dlg.setInputText(last_input.get());
+    dlg.setInputText("");
+
+    // Returns a list of tags [ { id: "tag id", name: "name", description: "Tag description"} ]
+    let token = window["generated_token"];
+    let all_tags = await tsutils.ajax_get("/api/tags", token);
+    console.log(all_tags);   
+    
+    for(let n in all_tags){
+        let row = all_tags[n];
+        console.log(" row = ", row);
+        console.log(` name = ${row[name]} - id = ${row["id"]}`)
+        dlg.add_option(row["name"], row["id"]);
+    }
+    
+    // Returns an object like: {  value: "Selected-value-from-list-box", key: 12515 }
+    let answer = await dlg.prompt_selected();
+    console.log(" ANSWER = ", answer);
+
+    let resp = null;
+    // last_input.set(answer["value"]);
+
+    if(answer["key"] == null)
+    {  
+        console.trace(" Create new TAG:");
+        // Create  new tag            
+        let payload = {   action:   "add_item_new_tag"
+                        , tag_name:  answer["value"]
+                        , item_id:   item_id 
+                      };
+        console.log(" Payload = ", payload);        
+        resp = await tsutils.ajax_request(HttpMethod.HTTP_PUT, "/api/tags", token, payload);
+
+    // Add a new item to a given tag 
+    } else {
+        let payload = {   action: "add_item"
+                        , tag_id:  answer["key"]
+                        , item_id: item_id 
+        };
+        console.log(" Payload = ", payload);
+        resp = await tsutils.ajax_request(HttpMethod.HTTP_PUT ,"/api/tags", token, payload);
+
+    }
+
+    console.log(" Resp = ", resp);
+
+    if(resp.status == 200 || resp.status == 201)
+    {
+        await Dialog_Notify.notify_ok("Tag added successfully. Ok.", 500);
+        dom.page_refresh();
+    } else {
+        await Dialog_Notify.notify_error(resp["message"], 500);
+    }
+
+}
+
+
+export 
+async function tag_remove(tag_id, bookmark_id)
+{    
+    let payload = {   action:   "remove_tag_item"
+                    , tag_id:    tag_id
+                    , item_id:  bookmark_id
+                };
+    console.log(" Payload = ", payload);
+    let token = window["generated_token"];
+    let resp = await tsutils.ajax_request(HttpMethod.HTTP_PUT, "/api/tags", token, payload);
+
+    if(resp["result"] == "OK")
+    {
+        await Dialog_Notify.notify_ok(resp["message"], 500);
+        dom.page_refresh(); 
+    } else {
+        await Dialog_Notify.notify_error(resp["message"], 500);
+    }
+}
+
+export 
+async function tag_delete(tag_name: string, tag_id: Number)
+{
+    let answer = await Dialog_YesNo.prompt(
+                    "Delete tag."
+                , `Are you sure you want to delete this tag: '${tag_name}'. 
+                   Note: This action is irreversible. ` );
+
+    if(!answer) { return; }
+
+    let resp = await tsutils.ajax_request( 
+                               HttpMethod.HTTP_PUT
+                            , "/api/tags"
+                            , window["generated_token"]
+                            , { 
+                                  "tag_name": tag_name 
+                                , "tag_id":   tag_id
+                                , "action":   "delete_tag"
+                                });
+
+
+    if(resp["result"] == "OK")
+    { 
+        Dialog_Notify.notify("Information", "Tag deleted. Ok.")
+        dom.page_refresh();
+    } else {
+        Dialog_Notify.notify("Error:", "Failed to delete tag.");                  
+    }
+}
+
+export 
+async function tag_update(tag_name: string, tag_id: Number, tag_desc: string)
+{
+    let dialog = new DialogForm();
+    //dialog_collection_edit.detach_on_close(false);
+    dialog.setTitle("Update tag");
+    dialog.setText("Enter the following informations:");
+    
+    let entry_name = dialog.add_row_input("name", "Tag Name:");
+    let entry_desc  = dialog.add_row_input("desc", "Tag Description:");
+
+    entry_name.value = tag_name;
+    entry_desc.value = tag_desc;
+    
+    // Wait user fill the dialog form information. 
+    let sender = await dialog.onConfirm();
+
+    let resp = await tsutils.ajax_request(
+                               tsutils.HttpMethod.HTTP_PUT
+                            , "/api/tags"
+                            , window["generated_token"]
+                            , { 
+                                  "action":         "update_tag"
+                                , "tag_name":        entry_name.value 
+                                , "tag_desc":        entry_desc.value 
+                                , "tag_id":          tag_id
+                                });
+
+    if(resp["result"] == "OK")
+    { 
+        Dialog_Notify.notify("Information", "Tag deleted. Ok.")
+        dom.page_refresh();
+    } else {
+        Dialog_Notify.notify("Error:", "Failed to delete tag.");                  
+    }    
+
+}
+
+export 
+async function tag_filter_window()
+{
+    let dlg = new Dialog_Datalist_Prompt();
+    dlg.setTitle("Select a tag");    
+    // dlg.setInputText(last_input.get());
+
+    // Returns a list of tags [ { id: "tag id", name: "name", description: "Tag description"} ]
+    let token = window["generated_token"];
+    let all_tags = await tsutils.ajax_get("/api/tags", token);
+    console.log(all_tags);   
+    
+    for(let n in all_tags){
+        let row = all_tags[n];
+        // console.log(" row = ", row);
+        // console.log(` name = ${row[name]} - id = ${row["id"]}`)
+        dlg.add_option(row["name"], row["id"]);
+    }    
+
+    // Returns an object like: {  value: "Selected-value-from-list-box", key: 12515 }
+    let answer = await dlg.prompt_selected();
+    console.log(" ANSWER = ", answer);
+
+    if(answer == null){ return; }
+
+    // Redirect URL 
+    dom.redirect_url( `/items?filter=tag-name&A0=${answer["value"]}` );
+}
 
 // window["item_quick_rename"] = item_quick_rename;
 
